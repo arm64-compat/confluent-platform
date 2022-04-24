ecdcca7779d8f1b8c872369195433c7a67722353
Anurag Agarwal <anurag.agarwal561994@gmail.com>
2022-04-24 14:39:04 +0530

    Corrects manifest script

--- create-manifest.sh
+++ create-manifest.sh
@@ -3,7 +3,7 @@
 DOCKER_DIR=$(dirname $1)
 DOCKER_DIR=$DOCKER_DIR/pom.xml
 
-ARTIFACT_ID=$(sh -c "$MVN_HELP -f common-docker/pom.xml -Dexpression=project.artifactId")
+ARTIFACT_ID=$(sh -c "$MVN_HELP -f $DOCKER_DIR -Dexpression=project.artifactId")
 
 DOCKER_IMAGE=$DOCKER_REGISTRY/$DOCKER_REPOSITORY/confluentinc/$ARTIFACT_ID
 
